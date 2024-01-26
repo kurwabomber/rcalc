367d2fe943c75c806de7bc8f4d46052bbc4b21bf
David Nguyen <davidWnguyen@proton.me>
2024-01-26 15:38:24 -0500

    its functioning lol

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
+use std::ops::Add;
+
 use iced::widget::text_input::Id;
-use iced::widget::{button, column, text, text_input};
+use iced::widget::{button, row, text, text_input};
 use iced::{Alignment, Element, Sandbox, Settings};
+use evalexpr::*;
 
 pub fn main() -> iced::Result {
     Calculator::run(Settings::default())
@@ -39,19 +42,29 @@ impl Sandbox for Calculator {
     fn update(&mut self, message: Message) {
         match message {
             Message::InputChanged(new_formula) => {
-                println!("changed the function to -> {}", new_formula);
                 self.formula.input = new_formula;
             }
         }
     }
 
     fn view(&self) -> Element<Message> {
-        column![
+        let expression = eval(&self.formula.input);
+        let output_text: String;
+        if expression.is_err(){
+            output_text = expression.unwrap_err().to_string();
+        }else{
+            output_text = ["= ", &expression.unwrap().to_string()].concat()
+        }
+
+        row![
             text_input(&self.formula.input, &self.formula.input)
                 .on_input(Message::InputChanged)
                 .id(self.formula.text_input_id.clone())
+                .width(600),
+            text(&output_text)
         ]
-        .padding(20)
+        .padding(50)
+        .spacing(100)
         .align_items(Alignment::Center)
         .into()
     }
